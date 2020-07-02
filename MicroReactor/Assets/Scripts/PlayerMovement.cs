3e2c9f6dc8bc6e64214e510c4ed782c381d9b91b
amberhh2 <amberhh2@illinois.edu>
2020-07-02 11:11:56 -0500

    Created first test scene "classroom" to test out ideas

---
+++ MicroReactor/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float directedForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("d")) //move right
+        {
+            rb.AddForce(directedForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a")) //move left
+        {
+            rb.AddForce(-directedForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w")) //move forward
+        {
+            rb.AddForce(0,0,directedForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s")) //move backward
+        {
+            rb.AddForce(0, 0, -directedForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
