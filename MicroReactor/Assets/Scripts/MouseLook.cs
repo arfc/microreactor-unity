891f0fd28c885e6f898284e7c272921399cb74a9
amberhh2 <amberhh2@illinois.edu>
2020-07-02 12:44:33 -0500

    Create first person controller

---
+++ MicroReactor/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f; //private variable; keep track of rotation about x axis (look up&down)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hide and lock cursor to center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //find position of mouse, take into consideration sensitivity and frame rate of computer
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; //each frame decrease x axis rotation by mouseY (if += instead, motion is flipped)
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //Clamping creates a boundary for looking up&down
+
+        //quaternion responsible for rotation in Unity and input Euler angles (rotation for (x, y, z) axis)
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); //apply rotation about x axis (look up&down)
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
--- MicroReactor/Assets/Scripts/PlayerMovement.cs
+++ MicroReactor/Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,59 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
 
-    public float directedForce = 500f;
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    //GroundCheck creates a sphere around a point used here to check for ground
+    public Transform GroundCheck; //variables to reset velocity   vvv
+    public float groundDistance = 0.4f; //radius of sphere with GroundCheck
+    public LayerMask groundMask; //control which objects the sphere checks for
+    bool isGrounded; //boolean with state
+
+    Vector3 velocity;
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(Input.GetKey("d")) //move right
+        //check if grounded, complete a physics check
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+        //reset velocity in y direction so it doesn't keep adding up
+        if (isGrounded && velocity.y < 0)
         {
-            rb.AddForce(directedForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            velocity.y = -2f; //not 0 in case isGrounded registers before player is fully grounded
         }
 
-        if (Input.GetKey("a")) //move left
-        {
-            rb.AddForce(-directedForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        if (Input.GetKey("w")) //move forward
-        {
-            rb.AddForce(0,0,directedForce * Time.deltaTime, ForceMode.VelocityChange);
-        }
+        //this moves using global coordinates, no direction player is facing
+        //Vector3 move = new Vector3(x, 0f, z);
 
-        if (Input.GetKey("s")) //move backward
+        //create direction to move based on x and z movement and with player face direction
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //move in direction of move vector w/ speed of player; independent of frame rate
+        controller.Move(move * speed * Time.deltaTime);
+
+        
+        //JUMPING
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.AddForce(0, 0, -directedForce * Time.deltaTime, ForceMode.VelocityChange);
+            //v=sqrt(h*-2*g)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+        
+
+        //free fall: change y = 1/2g*t^2
+        velocity.y += gravity * Time.deltaTime; //g*t
+        controller.Move(velocity * Time.deltaTime); //*t
     }
 }
